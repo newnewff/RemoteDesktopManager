28ea992b0fac21747e27061f4c103175d92df6a6
jfan01 <231283608@qq.com>
2021-07-21 09:59:01 +0800

    create

---
+++ RemoteDesktopManager/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace RemoteDesktopManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.a = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.start = new System.Windows.Forms.ToolStripMenuItem();
+            this.stop = new System.Windows.Forms.ToolStripMenuItem();
+            this.a.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.ContextMenuStrip = this.a;
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.Selected += new System.Windows.Forms.TabControlEventHandler(this.tabControl1_Selected);
+            // 
+            // a
+            // 
+            this.a.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.start,
+            this.stop});
+            this.a.Name = "a";
+            this.a.Size = new System.Drawing.Size(181, 70);
+            // 
+            // start
+            // 
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(180, 22);
+            this.start.Text = "连接";
+            // 
+            // stop
+            // 
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(180, 22);
+            this.stop.Text = "断开";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.a.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.ContextMenuStrip a;
+        private System.Windows.Forms.ToolStripMenuItem start;
+        private System.Windows.Forms.ToolStripMenuItem stop;
+    }
+}
+
---
+++ RemoteDesktopManager/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using MSTSCLib;
+
+namespace RemoteDesktopManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DESEncrypt.Key = "hghg";
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.Load("servicesInfo.xml");
+            //获取节点列表 
+            XmlNodeList topM = xmldoc.SelectNodes("//Service");
+            int index = 0;
+            AxMSTSCLib.AxMsTscAxNotSafeForScripting axIndex = null;
+            foreach (XmlElement element in topM)
+            {
+                string ip = element.GetElementsByTagName("IP")[0].InnerText;
+                string domain = element.GetElementsByTagName("Domain")[0].InnerText;
+                string username = element.GetElementsByTagName("UserName")[0].InnerText;
+                string password = element.GetElementsByTagName("PassWord")[0].InnerText;
+                string isMD5 = element.GetElementsByTagName("PassWordIsUseMD5")[0].InnerText;
+                if (isMD5 == "1")
+                {
+                    password = DESEncrypt.Decrypt(password);
+                }
+                TabPage tp = new TabPage();
+                tp.Tag = password;
+                tp.Text = ip;
+                tp.Name = "tp" + index;
+                AxMSTSCLib.AxMsTscAxNotSafeForScripting ax = new AxMSTSCLib.AxMsTscAxNotSafeForScripting();
+                ((System.ComponentModel.ISupportInitialize)(ax)).BeginInit();
+
+                tp.Controls.Add(ax);
+
+                ((System.ComponentModel.ISupportInitialize)(ax)).EndInit();
+                ax.Name = tp.Name + "ax";
+                ax.Server = ip;
+                ax.UserName = username;
+                ax.Domain = domain;
+                ax.Dock = DockStyle.Fill;
+                if (index == 0)
+                {
+                    axIndex = ax;
+                }
+
+                IMsTscNonScriptable secured = (IMsTscNonScriptable)ax.GetOcx();
+                secured.ClearTextPassword = password;
+
+                tabControl1.TabPages.Add(tp);
+
+
+
+
+
+                index++;
+            }
+            if (axIndex != null)
+            {
+                conn(axIndex);
+            }
+        }
+
+        private void tabControl1_Selected(object sender, TabControlEventArgs e)
+        {
+            AxMSTSCLib.AxMsTscAxNotSafeForScripting ax = (AxMSTSCLib.AxMsTscAxNotSafeForScripting)(e.TabPage.Controls.Find(e.TabPage.Name + "ax", false)[0]);
+
+            conn(ax);
+        }
+        private void conn(AxMSTSCLib.AxMsTscAxNotSafeForScripting ax)
+        {
+            if (ax.Connected == 0)
+            {
+                ax.Connect();
+            }
+        }
+
+        private void a_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            AxMSTSCLib.AxMsTscAxNotSafeForScripting ax = (AxMSTSCLib.AxMsTscAxNotSafeForScripting)(tabControl1.SelectedTab.Controls.Find(tabControl1.SelectedTab.Name + "ax", false)[0]);
+            if (e.ClickedItem.Name == "start")
+            {
+
+                conn(ax);
+            }
+            else
+            {
+                ax.Disconnect();
+            }
+        }
+
+        private void a_Opening(object sender, CancelEventArgs e)
+        {
+            AxMSTSCLib.AxMsTscAxNotSafeForScripting ax = (AxMSTSCLib.AxMsTscAxNotSafeForScripting)(tabControl1.SelectedTab.Controls.Find(tabControl1.SelectedTab.Name + "ax", false)[0]);
+
+            if (ax.Connected == 1)
+            {
+                a.Items[0].Enabled = false;
+                a.Items[1].Enabled = true;
+            }
+            else
+            {
+                a.Items[0].Enabled = true;
+                a.Items[1].Enabled = false;
+            }
+
+        }
+    }
+
+
+    public static class DESEncrypt
+    {
+        public static string Key { get; set; }
+        public static string Encrypt(string Text)
+        {
+            return Encrypt(Text, Key);
+        }
+        public static string Encrypt(string Text, string sKey)
+        {
+            //byte[] keyBytes = Encoding.UTF8.GetBytes(sKey.Substring(0, 8));
+            //byte[] keyIV = keyBytes;
+
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] inputByteArray;
+            inputByteArray = Encoding.Default.GetBytes(Text);
+            des.Key = ASCIIEncoding.ASCII.GetBytes(System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(sKey, "md5").Substring(0, 8));
+            des.IV = ASCIIEncoding.ASCII.GetBytes(System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(sKey, "md5").Substring(0, 8));
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            StringBuilder ret = new StringBuilder();
+            foreach (byte b in ms.ToArray())
+            {
+                ret.AppendFormat("{0:X2}", b);
+            }
+            return ret.ToString();
+        }
+
+        public static string Decrypt(string Text)
+        {
+            return Decrypt(Text, Key);
+        }
+        public static string Decrypt(string Text, string sKey)
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            int len;
+            len = Text.Length / 2;
+            byte[] inputByteArray = new byte[len];
+            int x, i;
+            for (x = 0; x < len; x++)
+            {
+                i = Convert.ToInt32(Text.Substring(x * 2, 2), 16);
+                inputByteArray[x] = (byte)i;
+            }
+            des.Key = ASCIIEncoding.ASCII.GetBytes(System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(sKey, "md5").Substring(0, 8));
+            des.IV = ASCIIEncoding.ASCII.GetBytes(System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(sKey, "md5").Substring(0, 8));
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            return Encoding.Default.GetString(ms.ToArray());
+        }
+    }
+}
